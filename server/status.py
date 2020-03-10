4aae4c0702a3a007a5326fe096461e3b74122e9d
leppan02 <leopold.hermansson@gmail.com>
2020-03-10 01:21:12 +0000

    baclup

--- server/status.py
+++ server/status.py
@@ -1,13 +1,20 @@
 import pickle
 
-with open('available.data', 'rb') as filehandle:
-    available = pickle.load(filehandle)
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+def calc():
+    with open('available.data', 'rb') as filehandle:
+        available = pickle.load(filehandle)
 
-with open('pending.data', 'rb') as filehandle:
-    pending = pickle.load(filehandle)
+    with open('pending.data', 'rb') as filehandle:
+        pending = pickle.load(filehandle)
 
-with open('done.data', 'rb') as filehandle:
-    done = pickle.load(filehandle)
+    with open('done.data', 'rb') as filehandle:
+        done = pickle.load(filehandle)
 
-print(pending)
-print(len(done)/(len(done)+len(available)+len(pending)))
+    print(pending)
+    print(len(done)/(len(done)+len(available)+len(pending)))
+    s.enter(5, 1, calc, ())
+
+s.enter(5, 1, calc, ())
+s.run()
